b35d016cb5f08c2d2a428c9e4cf7fa780e49a502
tomoya <wada-tomoya@webrage.jp>
2021-08-05 14:02:08 +0900

    [add]
    消費税計算のプッシュ

--- 02/tax.php
+++ 02/tax.php
@@ -67,8 +67,21 @@
             <input type="submit" value="送信" />　
             <input type="reset" value="取り消し" />
         </form>
-        <br>
+        <?php
+        $price1 = $_POST['nedan1'] * $_POST['kosuu1'];
+        $price1 = $price1 + $price1 * $_POST['tax1'] * 0.01;
+        $price2 = $_POST['nedan2'] * $_POST['kosuu2'];
+        $price2 = $price2 + $price2 * $_POST['tax2'] * 0.01;
+        $price3 = $_POST['nedan3'] * $_POST['kosuu3'];
+        $price3 = $price3 + $price3 * $_POST['tax3'] * 0.01;
+        $price4 = $_POST['nedan4'] * $_POST['kosuu4'];
+        $price4 = $price4 + $price4 * $_POST['tax4'] * 0.01;
+        $price5 = $_POST['nedan5'] * $_POST['kosuu5'];
+        $price5 = $price5 + $price5 * $_POST['tax5'] * 0.01;
+        $price_sum = $price1 + $price2 + $price3 + $price4 + $price5
 
+         ?>
+        <br>
         <table border="1" style="border-collapse:collapse;">
             <tr>
                 <th>商品名</th>
@@ -79,8 +92,38 @@
             </tr>
 
             <tr>
-                <td colspan="4" style="text-align:left;">合計</td>
-                <td><?php echo $price_sum;?></td>
+                <td><?php echo $_POST ['namae1'];?></td>
+                <td><?php echo $_POST ['nedan1'] . '円';?></td>
+                <td><?php echo $_POST ['kosuu1']  . '個';?></td>
+                <td><?php echo $_POST ['tax1'] . '%';?></td>
+                <td><?php echo $price1  . '円';?></td></tr>
+                <tr>
+                    <td><?php echo $_POST ['namae2'];?></td>
+                    <td><?php echo $_POST ['nedan2']  . '円';?></td>
+                    <td><?php echo $_POST ['kosuu2']  . '個';?></td>
+                    <td><?php echo $_POST ['tax2'] . '%';?></td>
+                    <td><?php echo $price2  . '円';?></td></tr>
+                    <tr>
+                        <td><?php echo $_POST ['namae3'];?></td>
+                        <td><?php echo $_POST ['nedan3']  . '円';?></td>
+                        <td><?php echo $_POST ['kosuu3']  . '個';?></td>
+                        <td><?php echo $_POST ['tax3'] . '%';?></td>
+                        <td><?php echo $price3  . '円';?></td></tr>
+                        <tr>
+                            <td><?php echo $_POST ['namae4'];?></td>
+                            <td><?php echo $_POST ['nedan4']  . '円';?></td>
+                            <td><?php echo $_POST ['kosuu4']  . '個';?></td>
+                            <td><?php echo $_POST ['tax4'] . '%';?></td>
+                            <td><?php echo $price4  . '円';?></td></tr>
+                            <tr>
+                                <td><?php echo $_POST ['namae5'];?></td>
+                                <td><?php echo $_POST ['nedan5']  . '円';?></td>
+                                <td><?php echo $_POST ['kosuu5']  . '個';?></td>
+                                <td><?php echo $_POST ['tax5'] . '%';?></td>
+                                <td><?php echo $price5  . '円';?></td></tr>
+
+                </td>
+
             </tr>
         </table>
   </body>
