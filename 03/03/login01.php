8040eef26188513eeba1e21cfc666d7841b8a29b
tomoya <wada-tomoya@webrage.jp>
2021-08-05 14:26:08 +0900

    [add]
    gitに追加

---
+++ 03/03/login01.php
@@ -0,0 +1,13 @@
+<?php
+
+  // idを設定
+  $id = "hogehoge";
+  // パスワードを設定
+  $password = "xxxxxx";
+
+  if($_POST['id'] == $id AND $_POST['password'] == $password){
+    /* ログイン成功の処理を書く */
+  } else {
+    /* ログイン失敗の処理を書く */
+  }
+?>
