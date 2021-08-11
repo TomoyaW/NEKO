d0d2c749cb43fb22556da97efa1c1210bcfd97c7
tomoya <wada-tomoya@webrage.jp>
2021-08-11 14:38:38 +0900

    [aad]
    05の修正

--- 04/arraytest01.php
+++ 04/arraytest01.php
@@ -1,23 +1,26 @@
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <title>配列の基本</title>
-    </head>
-    <body>
-        <h1>配列の基本</h1>
-        <?php
-            $fruit = array("りんご", "すいか", "みかん", "なし", "イチゴ", "かき");
-            var_dump($fruit);
-            echo "<hr>";
-            foreach($fruit as $each){
-                echo $each . "<br/>";
-            }
-            echo "<hr>";
-            foreach($fruit as $key => $value){
-                echo $key . "番目の要素は" . $value . "です。<br/>";
-        }
-        ?>
-    </body>
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <title>配列</title>
+</head>
+<body>
+    <h1>配列</h1>
+    <?php
+    $fruit = array("りんご", "すいか", "みかん", "なし", "イチゴ", "かき");
+    echo "<hr>";
+
+    foreach($fruit as $each){
+        echo $each . "<br/>";
+    }
+
+    echo "<hr>";
+
+    foreach($fruit as $key => $value){
+        echo $key . "番目の要素は" . $value . "です。<br/>";
+    }
+    ?>
+</body>
 </html>
