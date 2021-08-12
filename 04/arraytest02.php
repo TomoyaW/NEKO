e90ebf1cf854ca271b41fb8d77f837143755c211
tomoya <wada-tomoya@webrage.jp>
2021-08-12 11:01:28 +0900

    [add]
    arraytesut作成

--- 04/arraytest02.php
+++ 04/arraytest02.php
@@ -7,16 +7,23 @@
     </head>
     <body>
         <h1>配列の検索</h1>
-          <?php
-          $fruit = array("りんご", "すいか", "みかん", "なし", "イチゴ", "かき");
-          var_dump($fruit);
-          $needle = "すいか";
-          echo "<hr>";
-          if(in_array($needle, $fruit)) {
-              echo $needle . " がfruitの要素の値に存在しています";
-          } else {
-              echo $needle . " がfruitの要素の値に存在しません";
-          }
-          ?>
+        <?php
+        $fruit = array("りんご", "すいか", "みかん", "なし", "イチゴ", "かき");
+        var_dump($fruit);
+        echo "<hr>";
+        foreach($fruit as $each){
+          echo $each . "<br>";
+        }
+        echo "<hr>";
+        foreach($fruit as $key => $value){
+          echo $key . "番目の要素は" . $value . "です。<br>";
+            ｝
+            $needle = "みかん";
+            if(in_array($needle, $fruit)) {
+                 echo $needle . " がfruitの要素の値に存在しています";
+            } else {
+                echo $needle . " がfruitの要素の値に存在しません";
+            }
+        ?>
     </body>
 </html>
